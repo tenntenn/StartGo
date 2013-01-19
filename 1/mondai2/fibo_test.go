d0b05a83fae9a421f9c7778503632cd9be41972c
Takuya Ueda <uedatakuya@gmail.com>
2013-01-19 12:06:58 +0900

    問題2を追加した

---
+++ 1/mondai2/fibo_test.go
@@ -0,0 +1,21 @@
+package fibo
+
+import (
+	"testing"
+)
+
+func TestFibo(t *testing.T) {
+	expect := []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987}
+	fibo := Fibo()
+	i := 0
+	for v := range fibo {
+		if i >= len(expect) {
+			break
+		}
+		if v != expect[i] {
+			t.Errorf("v and expect[i] must be equal.")
+		}
+
+		i++
+	}
+}
